00bcfb47bc380298e741d1e1777c805445ff76cc
naveenchenamarla29 <naveensai.8055@gmail.com>
2025-05-11 01:02:44 +0530

    Initial commit

---
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab001_HelloWorld {
+
+    /*
+    main -> Main method
+    sout -> System.out.println();
+    new * -> ignore this
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/ex_01_Java_Basics/Lab002_No_Main_Method.java
@@ -0,0 +1,6 @@
+package ex_01_Java_Basics;
+
+public class Lab002_No_Main_Method {
+
+
+}
