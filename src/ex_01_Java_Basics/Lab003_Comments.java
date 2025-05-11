73a5774bacbd2b92556922e09700792fa36d1f49
naveenchenamarla29 <naveensai.8055@gmail.com>
2025-05-11 15:20:39 +0530

    Updates for Lab3 file

--- src/ex_01_Java_Basics/Lab003_Comments.java
+++ src/ex_01_Java_Basics/Lab003_Comments.java
@@ -2,10 +2,16 @@ package ex_01_Java_Basics;
 
 public class Lab003_Comments {
 
+    public static void main(String[] args) {
     //This is a single line comment. it will be ignored by the compiler
 
     /*
-    THis is a
+    This is a
     multi-line comment
+    and it will also be ignored
+    by the compiler
+    as this is also a comment
      */
+    }
+
 }
