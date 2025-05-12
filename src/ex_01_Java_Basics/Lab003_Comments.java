3b79dda3f7a6c4cbd187e4da52223845b957dfbf
naveenchenamarla29 <naveensai.8055@gmail.com>
2025-05-12 21:01:28 +0530

    Change for Lab003 and new Lab004 file

--- src/ex_01_Java_Basics/Lab003_Comments.java
+++ src/ex_01_Java_Basics/Lab003_Comments.java
@@ -4,14 +4,14 @@ public class Lab003_Comments {
 
     public static void main(String[] args) {
     //This is a single line comment. it will be ignored by the compiler
+        System.out.println("This is a single line comment");
 
-    /*
-    This is a
-    multi-line comment
-    and it will also be ignored
-    by the compiler
-    as this is also a comment
-     */
+    /*This is a multi-line comment
+    * and it will be ignored by
+    *  the compiler as well
+    */
+        System.out.println("This is a multi-line comment");
+
     }
 
 }
---
+++ src/ex_01_Java_Basics/Lab004CommonMath.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab004CommonMath {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(4/2); //Prints quotient
+        System.out.println(3/2); //Prints quotient. It prints integer values only but not 1.5
+        System.out.println(4%2); //Prints remainder
+        System.out.println(3%2); //Prints remainder
+    }
+}
